7222b106f77f88166e4b3b8d5b7e4f8b85c2dde1
Jackson Wu <jacksonwu226@gmail.com>
2024-06-06 11:53:03 -0700

    created preliminary ResumePreview component

---
+++ src/ResumePreview.jsx
@@ -0,0 +1,9 @@
+export default function ResumePreview({personalDetails}){
+  return(
+    <>
+      <p>{personalDetails.name}</p>
+      <p>{personalDetails.email}</p>
+      <p>{personalDetails.phoneNumber}</p>
+    </>
+  );
+}
